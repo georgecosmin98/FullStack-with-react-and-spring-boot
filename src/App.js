2f7b177e4345277ddf0935eebf3bfeab0d280f2a
georgecosmin98 <ilincageorgecomsin@yahoo.com>
2020-08-17 17:03:15 +0300

    Make Counter components file

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import FirstComponent from './components/learning-examples/FirstComponent';
 import SecondComponent from './components/learning-examples/SecondComponent';
 import ThirdComponent from './components/learning-examples/ThirdComponent';
+import Counter from './components/counter/Counter';
 import logo from './logo.svg';
 import './App.css';
 
@@ -9,6 +10,16 @@ class App extends Component{
   render(){
     return (
       <div className="App">
+        <Counter />
+      </div>
+    )
+  }
+}
+
+class LearningComponents extends Component{
+  render(){
+    return (
+      <div className="LearningComponents">
         Hello world! What you doing? :D 
         <FirstComponent />
         <SecondComponent/>
