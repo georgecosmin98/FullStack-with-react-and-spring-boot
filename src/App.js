a031ef2c18aed791cde2b3864ec847145874851d
georgecosmin98 <ilincageorgecomsin@yahoo.com>
2020-08-17 14:23:08 +0300

    class component vs function component

--- src/App.js
+++ src/App.js
@@ -7,9 +7,49 @@ class App extends Component{
     return (
       <div className="App">
         Hello world! What you doing? :D 
+        <FirstComponent />
+        <SecondComponent/>
+        <ThirdComponent/>
       </div>
     )
   }
 }
+
+
+//Class Component
+class FirstComponent extends Component{
+  render(){
+    return(
+      <div className="firstComponent">
+        <p>
+          First component!
+        </p>
+      </div>
+    )
+  }
+}
+
+//Class Component
+class SecondComponent extends Component{
+  render(){
+    return(
+      <div className="secondComponent">
+        <p>
+          Second component!
+        </p>
+      </div>
+    )
+  }
+}
+
+function ThirdComponent(){
+  return(
+    <div className="thirdComponent">
+      <p>
+        Third component!
+      </p>
+    </div>);
+}
+
 
 export default App;
