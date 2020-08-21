49c9b9453a015caf7e0fe6dd27839743c41b2e60
georgecosmin98 <ilincageorgecomsin@yahoo.com>
2020-08-21 16:09:05 +0300

    Adding state to login

--- src/components/todo/TodoApp.jsx
+++ src/components/todo/TodoApp.jsx
@@ -4,19 +4,47 @@ class TodoApp extends Component {
     render() {
         return (
             <div className="TodoApp">
-               <LoginComponent/>
-               </div>
+                <LoginComponent />
+            </div>
         )
     }
 }
 
 
 class LoginComponent extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: 'in28minutes',
+            password: 'aa'
+        }
+
+        this.handleUsernameChange = this.handleUsernameChange.bind(this);
+        this.handlePasswordChange = this.handlePasswordChange.bind(this);
+    }
+
+    handleUsernameChange(event) {
+        console.log(event.target.value);
+        this.setState(
+            {
+                username: event.target.value
+            }
+        )
+    }
+
+    handlePasswordChange(event){
+        console.log(event.target.value);
+        this.setState({
+            password: event.target.value
+        })
+    }
+
     render() {
         return (
             <div>
-                User name: <input type="text" name="username" />
-                Password: <input type="password" name="password" />
+                User name: <input type="text" name="username" value={this.state.username} onChange={this.handleUsernameChange} />
+                Password: <input type="password" name="password" value={this.state.password} onChange={this.handlePasswordChange} />
                 <button>Login</button>
             </div>
         )
