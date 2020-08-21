d750f28c6e62ea1cee7f874ab2aefc8d89a9c12c
georgecosmin98 <ilincageorgecomsin@yahoo.com>
2020-08-21 19:57:18 +0300

    Understanding conditional rendering with && in React JS

--- src/components/todo/TodoApp.jsx
+++ src/components/todo/TodoApp.jsx
@@ -54,8 +54,10 @@ class LoginComponent extends Component {
     render() {
         return (
             <div>
-                <ShowInvalidCredentials hasLoginFailed={this.state.hasLoginFailed} />
-                <ShowLoginSuccessMessage showSuccessMessage={this.state.showSuccessMessage} />
+                {/*<ShowInvalidCredentials hasLoginFailed={this.state.hasLoginFailed} />*/}
+                {this.state.hasLoginFailed && <div>Invalid Credentials</div>} {/*If condition is true => show message(div)*/ }
+                {/*<ShowLoginSuccessMessage showSuccessMessage={this.state.showSuccessMessage} />*/}
+                {this.state.showSuccessMessage && <div>Login Succesfully</div>}
                 User name: <input type="text" name="username" value={this.state.username} onChange={this.handleChange} />
                 Password: <input type="password" name="password" value={this.state.password} onChange={this.handleChange} />
                 <button onClick={this.loginClicked}>Login</button>
@@ -64,17 +66,17 @@ class LoginComponent extends Component {
     }
 }
 
-function ShowInvalidCredentials(props) {
-    if (props.hasLoginFailed) {
-        return <div>Invalid Credentials</div>
-    }
-    return null
-}
+// function ShowInvalidCredentials(props) {
+//     if (props.hasLoginFailed) {
+//         return <div>Invalid Credentials</div>
+//     }
+//     return null
+// }
 
-function ShowLoginSuccessMessage(props) {
-    if (props.showSuccessMessage) {
-        return <div>Login succesfully</div>
-    }
-    return null
-}
+// function ShowLoginSuccessMessage(props) {
+//     if (props.showSuccessMessage) {
+//         return <div>Login succesfully</div>
+//     }
+//     return null
+// }
 export default TodoApp
