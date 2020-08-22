ae13c7c9eca83606518bbc75d9a2de5958307a88
georgecosmin98 <ilincageorgecomsin@yahoo.com>
2020-08-22 13:54:10 +0300

    Using Bootstrap to create a menu with navigation links

--- src/components/todo/TodoApp.jsx
+++ src/components/todo/TodoApp.jsx
@@ -28,9 +28,19 @@ class TodoApp extends Component {
 class HeaderComponent extends Component {
     render() {
         return (
-            <div>
-                Header<hr />
-            </div>
+            <header>
+                <nav className="navbar navbar-expand-md navbar-dark bg-dark">
+                    <div><a href="https://google.com" className="navbar-brand">in28minutes</a></div>
+                    <ul className="navbar-nav">
+                        <li className="nav-link"><Link to="/welcome/in28minutes">Home</Link></li>
+                        <li className="nav-link"><Link to="/todos">Todos</Link></li>
+                    </ul>
+                    <ul className="navbar-nav navbar-collapse justify-content-end">
+                        <li className="nav-link"><Link to="/login">Login</Link></li>
+                        <li className="nav-link"><Link to="/logout">LogOut</Link></li>
+                    </ul>
+                </nav>
+            </header>
         )
     }
 }
