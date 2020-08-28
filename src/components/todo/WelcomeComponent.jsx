87cfcdc4714675d1666f036bcbfd503413273762
georgecosmin98 <ilincageorgecomsin@yahoo.com>
2020-08-28 15:41:35 +0300

    Adding axios

--- src/components/todo/WelcomeComponent.jsx
+++ src/components/todo/WelcomeComponent.jsx
@@ -1,11 +1,11 @@
 import React, { Component } from 'react'
-import {Link} from 'react-router-dom'
-
+import { Link } from 'react-router-dom'
+import HelloWorldService from '../api/todo/HelloWorldService'
 class WelcomeComponent extends Component {
-    
-    constructor(props){
+
+    constructor(props) {
         super(props)
-        this.retrieveWelcomeMEssage =this.retrieveWelcomeMEssage.bind(this)
+        this.retrieveWelcomeMEssage = this.retrieveWelcomeMEssage.bind(this)
     }
 
     render() {
@@ -15,16 +15,17 @@ class WelcomeComponent extends Component {
                 <div className="container">
                     Welcome {this.props.match.params.name}. You can manage your todos <Link to="/todos">here</Link>.
                 </div>
-                 <div className="container">
+                <div className="container">
                     Click here to get a customized welcome message.
-                    <button onClick={this.retrieveWelcomeMEssage} className= "btn btn-success">Get Welcome Messagee</button>
+                    <button onClick={this.retrieveWelcomeMEssage} className="btn btn-success">Get Welcome Messagee</button>
                 </div>
             </>
         )
     }
 
-    retrieveWelcomeMEssage(){
-        console.log('retrieve clicked')
+    retrieveWelcomeMEssage() {
+        HelloWorldService.executeHelloWorldService()
+            .then(response => console.log(response));
     }
 }
 
