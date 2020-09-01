20e2dacf5fd259ec3f2137a6690aca9498ac2f61
georgecosmin98 <ilincageorgecomsin@yahoo.com>
2020-09-01 18:55:10 +0300

    Creating Spring Boot REST API for retrieving Todo List

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/TodoHardcodedService.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/TodoHardcodedService.java
@@ -1,2 +1,27 @@
-package com.in28minutes.rest.webservices.restfulwebservices;public class TodoHardcodedService {
+package com.in28minutes.rest.webservices.restfulwebservices;
+
+import com.in28minutes.rest.webservices.restfulwebservices.todo.Todo;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Service
+public class TodoHardcodedService {
+
+    private static List<Todo> todos = new ArrayList();
+    private static int idCounter = 0;
+
+    static {
+        todos.add(new Todo(++idCounter, "in28minutes", "Learn to dance", new Date(), false));
+        todos.add(new Todo(++idCounter, "in28minutes", "Learn about Microservices", new Date(), false));
+        todos.add(new Todo(++idCounter, "in28minutes", "Learn about Angular", new Date(), false));
+
+    }
+
+    public List<Todo> findAll() {
+        return todos;
+    }
+
 }
