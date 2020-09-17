728b851439d8c88f56cb0d1a433b0c76bc3f3127
georgecosmin98 <ilincageorgecomsin@yahoo.com>
2020-09-17 15:55:31 +0300

    Creating Spring Boot REST API to delete a Todo

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/TodoResource.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/TodoResource.java
@@ -2,10 +2,8 @@ package com.in28minutes.rest.webservices.restfulwebservices;
 
 import com.in28minutes.rest.webservices.restfulwebservices.todo.Todo;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
 
@@ -19,5 +17,13 @@ public class TodoResource {
     @GetMapping("/users/{username}/todos")
     public List<Todo> getAllTodos(@PathVariable String username) throws InterruptedException {
         return todoService.findAll();
+    }
+
+    @DeleteMapping("/users/{username}/todos/{id}")
+    public ResponseEntity<Void> deleteTodo(@PathVariable String username, @PathVariable long id) {
+        Todo todo = todoService.deleteById(id);
+        if(todo!=null)
+            return ResponseEntity.noContent().build();
+        return ResponseEntity.notFound().build();
     }
 }
