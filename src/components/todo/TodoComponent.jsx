51ef4eb07527d4327126de8b35e81382b2e347c8
georgecosmin98 <ilincageorgecomsin@yahoo.com>
2020-09-21 19:26:13 +0300

    Initializing and handling form submit with Formik

--- src/components/todo/TodoComponent.jsx
+++ src/components/todo/TodoComponent.jsx
@@ -6,17 +6,27 @@ class TodoComponent extends Component {
         super(props)
         this.state = {
             id: 1,
-            description: 'Learn Forms',
+            description: 'Learn Forms Now',
             targetDate: moment(new Date()).format('YYYY-MM-DD')
         }
+
+        this.onSubmit=this.onSubmit.bind(this)
+    }
+
+    onSubmit(values){
+        console.log(values)
     }
 
     render() {
+        let {description,targetDate} = this.state   // let description = this.state.description && let targetDate = this.state.targetDate => JS Destructuring
         return (
             <div>
                 <h1>Todo</h1>
                 <div className="container">
-                    <Formik>
+                    <Formik
+                        initialValues={{description,targetDate}}
+                        onSubmit={this.onSubmit}
+                    >
                         {
                             (props) => (
                                 <Form>
