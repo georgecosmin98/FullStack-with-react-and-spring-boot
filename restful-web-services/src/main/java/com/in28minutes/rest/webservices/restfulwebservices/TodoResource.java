0fdcc106be5d0cdc776b183b06567867ce0a45ce
georgecosmin98 <ilincageorgecomsin@yahoo.com>
2020-09-22 19:38:13 +0300

    Creating Spring Boot REST API for Creating a Todo

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/TodoResource.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/TodoResource.java
@@ -5,7 +5,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.*;
 
 @RestController
@@ -36,7 +38,21 @@ public class TodoResource {
     @PutMapping("/users/{username}/todos/{id}")
     public ResponseEntity<Todo> updateTodo(@PathVariable String username, @PathVariable long id,
                                            @RequestBody Todo todo) {
-    Todo todoUpdated = todoService.save(todo);
-    return new ResponseEntity<Todo>(todo, HttpStatus.OK);
+        Todo todoUpdated = todoService.save(todo);
+        return new ResponseEntity<Todo>(todo, HttpStatus.OK);
+    }
+
+    @PostMapping("/users/{username}/todos")
+    public ResponseEntity<Void> updateTodo(@PathVariable String username,
+                                           @RequestBody Todo todo) {
+        Todo createdTodo = todoService.save(todo);
+
+        //Location
+        //Get current resource url
+        ///{id}
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}").buildAndExpand(createdTodo.getId()).toUri();
+
+        return ResponseEntity.created(uri).build();
     }
 }
